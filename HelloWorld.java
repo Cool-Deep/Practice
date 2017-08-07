f109afe82f84988f37be734858d33f708337003e
Malay <malaypatel6180@gmail.com>
2017-08-07 14:14:26 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
   public static void main(String[] args) {
     System.out.println("Hello Malay, Build was Successfully executed");
+    System.out.println("I love coding");
     System.out.println("Jenkins build data successfully ececuted!!!!");
   }
 }
